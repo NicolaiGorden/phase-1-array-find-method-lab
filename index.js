ae47a91321aea3c7230e94b580e42069101b8c71
Nicolai Gorden <nicolaigorden@gmail.com>
2022-01-14 19:22:52 -0600

    commit tree b6ed02f519bdfb31cfbd122144b3dc3be30e41f6

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin (records) {
+    const findWin = records.find(records => records.result === "W")
+    if (!!findWin) {
+      return findWin.year
+    } else {
+      return undefined
+    }
+  }
